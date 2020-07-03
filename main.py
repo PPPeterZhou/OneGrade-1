d74106948fab3779ad734ee940cbf15683785e42
PPPeterZhou <44284614+PPPeterZhou@users.noreply.github.com>
2020-07-03 13:53:14 -0600

    a

--- main.py
+++ main.py
@@ -42,6 +42,10 @@ class OneGrade(QMainWindow):
             'Course Number: ')
 
         if course_number_ok:
+            course_target, course_target_ok  = QInputDialog.getText(self, 'Course Infomation', 
+            'Course Target: ')
+        
+        if course_target_ok:
             course_credit, course_credit_ok  = QInputDialog.getText(self, 'Course Infomation', 
             'Course Credit(s): ')
 
@@ -50,13 +54,13 @@ class OneGrade(QMainWindow):
             langs =[str(year) + 'Winter', str(year) + 'Spring', str(year) + 'Summer', str(year) + 'Fall', str(year+1) + 'Winter', str(year+1) + 'Spring', str(year+1) + 'Summer', str(year+1) + 'Fall'] 
             course_session, course_session_ok = QtWidgets.QInputDialog.getItem(self, 'Course Infomation', 'Course Session:', langs) 
 
-        course_info = [course_code, course_number, course_credit, course_session]
+        course_info = [course_code, course_number, course_target, course_credit, course_session]
         # print('Course Code: %s\nCourse Number: %s\nCredit(s): %s \nSession: %s \n' % (course_code, course_number, course_credit, course_session))
         # print(course_info)
         self.storeCourseInfo(course_info)
 
     def storeCourseInfo(self, course_info):
-        self.db.insert_course(course_info[0], course_info[1], course_info[2], course_info[3])
+        self.db.insert_course(course_info[0], course_info[1], course_info[2], course_info[3], course_info[4])
         
 
 if __name__ == '__main__':
