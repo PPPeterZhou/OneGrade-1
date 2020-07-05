15e40c36af1c7b5815dbec844c9360a91b5a031d
Yanyu Zhang <45041565+Yanyu080500@users.noreply.github.com>
2020-07-05 15:29:51 -0600

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -19,14 +19,14 @@ class analyzer():
             print(self.comment_format.format("You need at least {0:.3}% to achieve your goal.".format(grade_needed)))
         elif grade<self.target_grade:
             lostmark = (100-grade)*weight/100
-            print(self.comment_format.format("You have lost {0}% of the total mark in this component.".format(lostmark)))
+            print(self.comment_format.format("You have lost {0:.3}% of the total mark in this component.".format(lostmark)))
         else:
             lostmark = (100-grade)*weight/100
-            print(self.comment_format.format("You are doing well, you only lost {0} percent in this part.".format(lostmark)))
+            print(self.comment_format.format("You are doing well, you only lost {0:.3}% in this part.".format(lostmark)))
 
     def comment_course(self):
         if self.total_lost_marks >= (100-self.target_grade):
-            print(" Your goal is unrealistic, you are {0}% behind of your goal.\n".format(self.traget_grade-(100-self.total_lost_marks)))
+            print(" Your goal is unrealistic, you are {0:.3}% behind of your goal.\n".format(self.traget_grade-(100-self.total_lost_marks)))
         else:
             print(" Your goal is achievable, you have lost {0:.3}% of the total grade.".format(self.total_lost_marks))
         print(" Your current average is {0:.3%}.\n".format(self.total_mark/self.total_weight))
